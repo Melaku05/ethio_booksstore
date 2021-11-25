2c433172fcee847e5a257018f39dfd0eb3b447fe
melaku <melakushiferaw05@gmail.com>
2021-11-25 15:25:56 +0300

    Setup static files, bootstrap, create a home app, and finally collect static files and test those files.

--- ethio_booksstore/settings.py
+++ ethio_booksstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'home.apps.HomeConfig',
+    'bootstrap',
+    'fontawesome',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +56,7 @@ ROOT_URLCONF = 'ethio_booksstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ home/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+''' HomePageTests class check that the HTTP status code for the homepage equals
+200 which means that it exists.'''
+class HomePageTests(SimpleTestCase):
+
+    '''we’re creating a variable
+called response that accesses the homepage (/) and then uses Python’s assertEqual
+to check that the status code matches 200 '''
+    def test_homepage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_homepage_url(self):
+        response = self.client.get(reverse('home')) #we are calling the URL name of home via the reverse method
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):   #SimpleTestCase comes with a method assertTemplateUsed for testing templates
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+        '''We’ve created a response variable  and then checked that the template home.html
+is used.'''
+
+    def test_homepage_html(self):  # new
+        response = self.client.get('/')
+        self.assertContains(response, 'Ethio Book Store')
+
+
+
+
