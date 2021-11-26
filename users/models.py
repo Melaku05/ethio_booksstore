98013299919bbae5a3632e8cbb3a9d31dda9ed5f
melaku <melakushiferaw05@gmail.com>
2021-11-26 09:55:18 +0300

    we make corrections in the comments and indentations.

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+#Creating a new CustomUser model which extends AbstractUser
 class CustomUser(AbstractUser):
     pass
 # Create your models here.
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='kebede',
+            email='kebede@email.com',
+            password='passworduser'
+        )
+        self.assertEqual(user.username, 'kebede')
+        self.assertEqual(user.email, 'kebede@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='passwordsuperuser'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
