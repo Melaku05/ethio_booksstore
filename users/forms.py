25c0da3674b359e9c5b50407df873220f6956fa6
melaku <melakushiferaw05@gmail.com>
2021-11-26 10:07:19 +0300

    we make corrections in the comments and indentations.

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+'''imported CustomUser model via get_user_model which looks
+to our AUTH_USER_MODEL config in settings.py'''
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
